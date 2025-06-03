8aa7b1c246b652518e72a4c1e650df518a443dca
Farhan <kaaf73636@gmail.com>
2025-06-03 13:16:10 +0530

    #changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
     const tl = gsap.timeline();
 
     tl.to(".vi-mask-group", {
-      rotate: 45,
-      duration: 1.8,
+      rotate: 70,
+      duration: 1.4,
       ease: "Power4.easeInOut",
       transformOrigin: "50% 50%",
     }).to(".vi-mask-group", {
-      scale: 10,
-      duration: 2,
-      delay: -1.9,
+      scale: 20,
+      duration: 2.4,
+      delay: -1.8,
       ease: "Expo.easeInOut",
       transformOrigin: "60% 60%",
       opacity: 0,
@@ -29,7 +29,23 @@ function App() {
         }
       },
     });
+
+     // Animate Rockstar logo: bottom-left → bottom-center
+   gsap.to(".rock-logo", 
+    {
+      left: "50%", 
+      scale: 1.7,
+      x: "-50%", 
+      filter: "blur(2px)",
+      duration: 4.8,
+      ease: "expo.out"
+    });
+
+ 
+
   });
+
+
 
 useGSAP(() => {
   if(!showContent) return;
@@ -58,51 +74,72 @@ useGSAP(() => {
     ease: "Expo.easeInOut",
   });
 
-   gsap.to(".character", {
-    scale: .8,
-    x: "-50%",
-    bottom: "-25%",
-    rotate: 0,
-    duration: 2,
-    delay: "-.8",
-    ease: "Expo.easeInOut",
+  gsap.to(".character", {
+    bottom: "80%",       // Center vertically
+    translateY: "100%",
+    x: "0%",  // Align center
+    duration: 4,
+    ease: "power4.out",
   });
 
    gsap.to(".logo1", {
-     left: "50%", // Move to horizontal center
+    bottom: "30%", // Move to horizontal center
   transform: "translate(-50%, -50%)",
   duration: 2,
   ease: "power3.out",
   onComplete: () => {
     const el = document.querySelector(".logo1");
-    el.style.left = "50%";
+    el.style.bottom = "30%";
     el.style.transform = "translate(-50%, -50%)";
     el.style.transition = "none";
   },
   });
 
+  gsap.to(".helipad", {
+    top: "20%",
+    y: "-50%",
+    duration: 2,
+    ease: "power4.out",
+    position: "absolute",
+  });
+
+  gsap.to(".logo18", {
+    bottom: "32%",
+    x: "150%",
+    y: "0%",
+    duration: 4,
+    ease: "power4.out",
+  });
+
    gsap.to(".logo2", {
   y: "-=20", // Move up
-  duration: 1.2,
+  duration: 1.4,
   repeat: -1,
   yoyo: true,
   ease: "sine.inOut",
   }); 
 
   gsap.to(".logo2", {
-  top: "50%",
+  top: "35%",
   transform: "translate(-50%, -50%)", // center it perfectly
-  duration: 2,
+  duration: 1.5,
   ease: "power3.out",
   onComplete: () => {
     const el = document.querySelector(".logo2");
 
     // Clean up inline styles so position is permanent
-    el.style.top = "50%";
+    el.style.top = "35%";
     el.style.transform = "translate(-50%, -50%)";
     el.style.transition = "none";
   }
   });  
+
+  gsap.to(".rock", {
+    left: "90%",       // Move to the right
+    x: "-50%",         // Slight adjustment to keep anchor centered
+    duration: 4,
+    ease: "power3.out",
+  });
 
    gsap.to(".rimg", {
    opacity: 1,
@@ -119,13 +156,6 @@ useGSAP(() => {
   yoyo: true,
   ease: "sine.inOut",
   });
-
-
-
-
-  
-
-
 
   const main = document.querySelector(".main");
 
@@ -160,7 +190,13 @@ useGSAP(() => {
 
   return (
     <>
-      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-black">
+        <img
+          src="./rock.png"
+          alt="Rockstar Logo"
+          className="rock-logo absolute left-[5%] bottom-[5%] w-[60px]"
+        />
+        
         <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
           <defs>
             <mask id="viMask">
@@ -192,6 +228,9 @@ useGSAP(() => {
       </div>
       {showContent && (
         <div className="main w-full rotate-[-25deg] scal-[1.7]">
+
+          {/*First Landing page */}
+
           <div className="landing w-full h-screen bg-black">
             <div className="navbar absolute top-0 left-0 z-10 w-full py-10 px-10">
               <div className="logo flex gap-7">
@@ -203,22 +242,33 @@ useGSAP(() => {
                 <h3 className="text-5xl -mt-[13px] leading-none text-white">Rockstar</h3>
               </div>
             </div>
+            
+            {/*Image Section */}
+
             <div className="imagesdiv overflow-hidden relative items-center w-full h-screen">
               <img className="absolute sky scale-[1.4] rotate-[-20deg] top-0 left-0 w-full h-full object-cover" src="./sky.png" alt="" />
               <img className="absolute BG scale-[1.4] rotate-[-5deg] top-0 left-0 w-full h-full object-cover" src="./BG (2).png" alt="" />
-               <img className="absolute logo2 w-150 h-150 -top-70 left-1/2 -tranlate-x-1/2 -translate-y-1/2 scale-[1.4]" src="./logo2.png" alt="" />
-              <img className="absolute logo1 w-100 h-100 top-70 -right-110 -tranlate-x-1/2 scale-[1.4] " src="./logo1.png" alt="" />
-              <img className="absolute character top-25 left-1/2 -translate-x-1/2 scale-[0.9] rotate-[-20deg] " src="./character.png" alt="" />
+              <img className="absolute logo2 w-150 h-150 -top-70 left-1/2 -tranlate-x-1/2 -translate-y-1/2 scale-[1.4]" src="./logo2.png" alt="" />
+              <img className="absolute helipad top-[-500px] right-1/2 -tranlate-x-1/2 w-80 h-80 " src="./helipad.png" alt="" />
+              <img className="absolute logo1 w-100 h-100 -bottom-10 left-1/2 -tranlate-x-1/2 scale-[1.4] " src="./logo1.png" alt="" />
+              <img className="absolute character bottom-[-100%] left-1/2 -translate-x-1/2 w-140" src="./character.png" alt="" />
+              <img className="logo18 absolute bottom-[-100px] left-1/2 -translate-x-1/2 w-30 h-30" src="./18+.png" alt="" />
             </div>
-             <div className="btmbar absolute text-white bottom-229 left-0 w-full py-15 px-10 bg-gradient-to-t from-black to-transparent">
-              <div className="flex gap-4 items-center">
+
+            {/*Bottom Bar */}
+            
+            <div className="btmbar absolute text-white bottom-229 left-0 w-full py-15 px-10 bg-gradient-to-t from-black to-transparent">
+              <div className="flex gap-2 items-center">
               <i className="text-4xl ri-arrow-down-line"></i>
               <h3 className="text-2xl font-[Helvetica_Now_Display]">Scroll Down</h3>
               </div>
               <img className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[55px]" src="./ps5.png" alt="" />
+              <img className="rock absolute bottom-10 py-2 px-2 top-7 left-1/2 w-30 h-30" src="./rock.png" alt="" />
             </div>
-           
           </div>
+
+          {/*Second Landing Page*/}
+
           <div className="w-full h-screen px-10 flex flex-col items-center justify-center overflow-hidden bg-black">
             <div className="imagediv2 relative scale-[1.3] w-full h-screen">
                <img className="absolute BG2 top-0 left-0 w-full h-full object-cover" src="./BG2.png" alt="" />
